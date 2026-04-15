333a11369e7ed3f8f14b8d4e630a2c979a59a0d6
RIG360 <134787168+RIG360@users.noreply.github.com>
2026-04-15 12:41:41 +0200

    upload

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bild_1",
+      "name": "Bild_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 2.9450268398240134,
+        "pitch": -0.005038721027606385,
+        "fov": 1.4723711838569904
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.02274745126085,
+          "pitch": 0.045185629985279974,
+          "rotation": 0,
+          "target": "1-bild_2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-bild_2",
+      "name": "Bild_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.4723711838569904
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9162210578453465,
+          "pitch": -0.011357895406041152,
+          "rotation": 0,
+          "target": "0-bild_1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Horm58_260408",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
